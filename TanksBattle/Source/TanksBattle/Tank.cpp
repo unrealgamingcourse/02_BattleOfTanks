c4655b4c69ce9f79a3f0f8c437149b89dfc4cf39
NISHANTVARSHNEY\Nishant Varshney <nishant26varshney@yahoo.com>
2017-07-26 18:42:34 +0530

    5

---
+++ TanksBattle/Source/TanksBattle/Tank.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Tank.h"
+#include "Engine.h"
+
+
+// Sets default values
+ATank::ATank()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ATank::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ATank::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ATank::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	
+	InputComponent->BindAction("Turret_Rotate", IE_Pressed, this, &ATank::RotateTurret);
+
+}
+
+void ATank::RotateTurret()
+{
+	UE_LOG(LogTemp, Warning, TEXT("SetupPlayerInputComponent called!"));
+}
+
+
