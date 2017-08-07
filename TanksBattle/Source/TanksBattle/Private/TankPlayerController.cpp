708c194b9f0ca5dabdf6908b2442e70ef7a39cd8
NISHANTVARSHNEY\Nishant Varshney <nishant26varshney@yahoo.com>
2017-08-08 00:39:26 +0530

    7.1

---
+++ TanksBattle/Source/TanksBattle/Private/TankPlayerController.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "TanksBattle.h"
+
+
+
+ATanks* ATankPlayerController::GetControlledTank() const
+{
+	return Cast<ATanks>(GetPawn());
+}
+
---
+++ TanksBattle/Source/TanksBattle/Public/TankPlayerController.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Tanks.h"
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "TankPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TANKSBATTLE_API ATankPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+
+	ATanks* GetControlledTank() const;
+	
+	
+};
